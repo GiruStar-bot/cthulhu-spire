258796f4f43a0af068b99126223985ed9e497274
SOICHIRO <a5402s2004@gmail.com>
2026-09-12 19:23:07 +0800

    Continue ritual pixel UI rollout

--- src/components/ui/PixelWindow.tsx
+++ src/components/ui/PixelWindow.tsx
@@ -4,7 +4,7 @@ import { cn } from "@/lib/utils";
 export function PixelWindow({ className, children, ...props }: HTMLAttributes<HTMLDivElement>) {
   return (
     <div
-      className={cn("panel frame-panel p-3 font-pixel text-white", className)}
+      className={cn("stone-panel frame-panel p-3 font-pixel text-white", className)}
       {...props}
     >
       {children}
